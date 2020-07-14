7f24943f712450712e059509bf685dd8b8c8b1e8
gokul nath <userinterface18@gmail.com>
2020-07-14 10:40:49 +0530

    pagedone

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-const express = require('express')
+const express = require("express")
 const app = express()
-const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// use the express-static middleware
+app.use(express.static("public"))
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+// define the first route
+app.get("/hello", function (req, res) {
+    res.send("<h1>Hello World!</h1>")
+})
+
+// start the server listening for requests
+app.listen(process.env.PORT || 3000,
+    () => console.log("Server is running..."));
